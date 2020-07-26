1ac845683355feae56e05b65e8ae52a44bc9aeee
John Ranck <jranck333@gmail.com>
2020-07-26 03:29:51 -0500

    second stranger

---
+++ Assets/Scripts/Perspective1.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perspective1 : MonoBehaviour
+{
+
+
+    [SerializeField] GameObject player;
+    //public static SortingLayer Streetlight2;
+    // public static GameObject lamp1;
+    //public int sortingOrder = 0;
+    public SpriteRenderer sprite;
+    public GameObject pivot;
+
+    //public string sortingLayerName { get; private set; }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Perspective();
+
+    }
+
+    public void Perspective()
+    {
+        if (player.transform.position.y > pivot.transform.position.y)
+        {
+            //  Debug.Log("good");
+            sprite.sortingOrder = 1;
+            // gameObject.layer = LayerMask.NameToLayer("Streetlight");
+        }
+        else
+        {
+            sprite.sortingOrder = 0;
+        }
+    }
+}
