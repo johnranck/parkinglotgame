6d4781985215b922c5be59c72787d7cf43681a2f
John Ranck <jranck333@gmail.com>
2020-07-26 05:09:43 -0500

    pause works okay

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+
+
+
+    [SerializeField] public GameObject pausePanel; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //pausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+       void Update()
+       {
+
+
+           if (Input.GetKeyDown(KeyCode.Escape))
+           {
+            Debug.Log("first"); 
+               if (!pausePanel.activeInHierarchy)
+               {
+                Debug.Log("second"); 
+                   PauseGame();
+               }
+
+           else if (pausePanel.activeInHierarchy)
+            {
+                UnPauseGame();
+                Debug.Log("third");
+               
+            }
+
+           }
+ 
+       }
+
+    public  void  PauseGame()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true); 
+    }
+
+   public  void  UnPauseGame()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/Resume.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Resume : MonoBehaviour
+{
+
+    int n;
+    public Pause paused; 
+
+    public void OnButtonPress()
+    {
+        n++; 
+        Debug.Log("Button clicked " + n + "times");
+        paused.UnPauseGame(); 
+       // GetComponent<Pause>().UnPauseGame(); 
+    }
+
+}
+
+
